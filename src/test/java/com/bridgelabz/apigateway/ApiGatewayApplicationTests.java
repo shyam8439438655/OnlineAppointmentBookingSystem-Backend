5b9c4fe0d7ecab2635ad11c6fb7ba641eec88480
shyamparashar42@gmail.com <shyamparashar42@gmail.com>
2026-05-16 00:15:37 +0530

    push isolated api-gateway

---
+++ src/test/java/com/bridgelabz/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+        org.junit.jupiter.api.Assertions.assertTrue(true, "Context should load successfully");
+    }
+
+}
